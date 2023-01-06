7c4c0e55d145f82929b7238b4819ae5467317745
suzek-klf <suzeklf@gmail.com>
2023-01-06 14:24:13 -0500

    show list

--- src/pages/Activity_Feed.jsx
+++ src/pages/Activity_Feed.jsx
@@ -1,5 +1,18 @@
 import React, { Component } from "react";
 import { getAllActivitiesUrl } from "../utils/api";
+import axios from "axios";
+import { MAIN_GREEN } from "../utils/constants";
+import {
+  Alert,
+  Space,
+  Spin,
+  Avatar,
+  Button,
+  List,
+  Skeleton,
+  Divider,
+} from "antd";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const ActivityFeed = () => {
   const [list, setList] = React.useState([]);
@@ -8,11 +21,11 @@ const ActivityFeed = () => {
   React.useEffect(() => {
     setLoading(true);
     console.log(getAllActivitiesUrl);
-    fetch(getAllActivitiesUrl)
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setList(data);
+    axios
+      .get(getAllActivitiesUrl)
+      .then((response) => {
+        console.log(response.data);
+        setList(response.data);
       })
       .catch((err) => {
         // Handle errors
@@ -21,7 +34,46 @@ const ActivityFeed = () => {
     setLoading(false);
   }, []);
 
-  return <div>Activity Feed</div>;
+  return (
+    <div className="feed">
+      {loading ? (
+        <Spin tip="Loading" size="large">
+          <div className="content" />
+        </Spin>
+      ) : (
+        <InfiniteScroll
+          dataLength={list.length}
+          // next={loadMoreData}
+          hasMore={list.length < 19}
+          loader={
+            <Skeleton
+              avatar
+              paragraph={{
+                rows: 1,
+              }}
+              active
+            />
+          }
+          endMessage={<Divider plain>It is all, nothing more 🤐</Divider>}
+          scrollableTarget="scrollableDiv"
+        >
+          <List
+            dataSource={list}
+            renderItem={(item) => (
+              <List.Item key={item.id}>
+                <List.Item.Meta
+                  // avatar={<Avatar src={item.picture.large} />}
+                  title={<a href="https://ant.design">{item.from}</a>}
+                  description={item.call_type}
+                />
+                <div>Content</div>
+              </List.Item>
+            )}
+          />
+        </InfiniteScroll>
+      )}
+    </div>
+  );
 };
 
 export default ActivityFeed;
